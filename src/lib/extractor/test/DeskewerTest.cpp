6814b4740e480401ac3497b540af8668668b1058
sz <sz@galacticicecube.com>
2020-04-09 14:21:30 -0500

    Initial commit -- stub project

---
+++ src/lib/extractor/test/DeskewerTest.cpp
@@ -0,0 +1,12 @@
+#include "unittest.h"
+
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+TEST_CASE( "DeskewerTest/testSimple", "[unit]" )
+{
+	assertEquals(true, false);
+}
+
---
+++ test/unittest.h
@@ -0,0 +1,13 @@
+#include "catch.hpp"
+
+#define assertMsg(expr,msg) {bool res = expr; if(!res) FAIL(msg); REQUIRE(res);}
+#define assertTrue(expr) REQUIRE(expr)
+#define assertFalse(expr) REQUIRE_FALSE(expr)
+#define assertEquals(x,y) REQUIRE((x) == (y))
+#define assertMatch(x,y) assertMsg(std::regex_match(y, std::regex(x)), x + std::string(" does not match ") + y)
+#define assertIn(x,y) REQUIRE(std::find(y.begin(), y.end(), x) != y.end())
+#define assertStringsEqual(x,y) REQUIRE(std::string(x) == std::string(y))
+#define assertStringContains(x,y) assertMsg(std::string(y).find(x) != std::string::npos, std::string(y) + " did not contain " + std::string(x))
+#define assertInRange(x,y,z) REQUIRE(x <= y); REQUIRE(y <= z)
+#define assertNull(expr) REQUIRE(expr == (void*)NULL)
+#define assertNotNull(expr) REQUIRE(expr != (void*)NULL)
