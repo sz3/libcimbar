fb5e11e6fca93348e7f849b2d199c8466301a000
sz <sz@galacticicecube.com>
2020-04-16 00:41:20 -0500

    Cell drift tracker, pt1

---
+++ src/lib/cimb_translator/CellDrift.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <vector>
+#include <utility>
+
+template <unsigned _limit=2>
+class CellDrift
+{
+public:
+	CellDrift() {}
+
+	const std::vector<std::pair<int, int>> driftPairs() const
+	{
+		static std::vector<std::pair<int, int>> drift = {
+			{0, 0}, {1, 0}, {0, 1}, {-1, 0}, {0, -1}, {1, 1}, {-1, -1}, {1, -1}, {-1, 1}
+		};
+		return drift;
+	}
+
+	int x() const
+	{
+		return _x;
+	}
+
+	int y() const
+	{
+		return _y;
+	}
+
+	void updateDrift(int dx, int dy)
+	{
+		_x += dx;
+		_y += dy;
+		if (_x > _limit)
+			_x = _limit;
+		else if (_x < 0-_limit)
+			_x = 0-_limit;
+		if (_y > _limit)
+			_y = _limit;
+		else if (_y < 0-_limit)
+			_y = 0-_limit;
+	}
+
+protected:
+	int _x = 0;
+	int _y = 0;
+};
--- src/lib/cimb_translator/CimbReader.cpp
+++ src/lib/cimb_translator/CimbReader.cpp
@@ -1,5 +1,7 @@
 #include "CimbReader.h"
 
+#include "CellDrift.h"
+
 const unsigned cellSize = 8; // goes into settings?
 
 CimbReader::CimbReader(std::string filename)
@@ -20,7 +22,11 @@ unsigned CimbReader::read()
 	if (_position.done())
 		return 0;
 
+	CellDrift drift;
 	CellPosition::coordinate xy = _position.next();
+	xy.first += drift.x();
+	xy.second += drift.y();
+
 	cv::Rect crop(xy.first, xy.second, cellSize, cellSize);
 	cv::Mat cell = _image(crop);
 	return _decoder.decode(cell);
