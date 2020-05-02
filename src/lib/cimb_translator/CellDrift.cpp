1e2d1d0d2540872b4ec6f9292ccbe315a0e6e3e8
sz <sz@galacticicecube.com>
2020-05-02 00:07:30 -0500

    Minor improvements, maybe, for CellDrift class

--- src/lib/cimb_translator/CellDrift.cpp
+++ src/lib/cimb_translator/CellDrift.cpp
@@ -1,15 +1,8 @@
 #include "CellDrift.h"
-#include <vector>
-#include <utility>
 
 CellDrift::CellDrift(int limit)
     : _limit(limit)
 {}
-
-const std::vector<std::pair<int, int>> CellDrift::driftPairs = {
-    //{0, 0}, {1, 0}, {0, 1}, {-1, 0}, {0, -1}, {1, 1}, {-1, -1}, {1, -1}, {-1, 1}
-    {-1, -1}, {0, -1}, {1, -1}, {-1, 0}, {0, 0}, {1, 0}, {-1, 1}, {0, 1}, {1, 1}
-};
 
 int CellDrift::x() const
 {
@@ -24,13 +17,9 @@ int CellDrift::y() const
 void CellDrift::updateDrift(int dx, int dy)
 {
 	_x += dx;
+	_x = std::min(_x, _limit);
+	_x = std::max(_x, 0-_limit);
 	_y += dy;
-	if (_x > _limit)
-		_x = _limit;
-	else if (_x < (0-_limit))
-		_x = 0-_limit;
-	if (_y > _limit)
-		_y = _limit;
-	else if (_y < (0-_limit))
-		_y = 0-_limit;
+	_y = std::min(_y, _limit);
+	_y = std::max(_y, 0-_limit);
 }
