804342684e20fafb20884fb3ac910f5b0d6ad5f3
sz <sz@galacticicecube.com>
2020-05-20 01:02:04 -0500

    Make Decoder header-only, which means the module is now header-only...
    seems like `add_library(foo INTERFACE)` is what most people recommend,
    so I'll use that instead of add_custom_target()

--- src/lib/encoder/Decoder.h
+++ src/lib/encoder/Decoder.h
@@ -1,6 +1,10 @@
 #pragma once
 
+#include "reed_solomon_stream.h"
+#include "bit_file/bitwriter.h"
 #include "cimb_translator/CimbDecoder.h"
+#include "cimb_translator/CimbReader.h"
+#include "cimb_translator/Config.h"
 #include <opencv2/opencv.hpp>
 #include <string>
 
@@ -13,7 +17,67 @@ public:
 	unsigned decode(std::string filename, std::string output);
 
 protected:
+	unsigned do_decode(CimbReader& reader, unsigned ecc_bytes, std::string output, unsigned bits_per_op);
+
+protected:
 	unsigned _eccBytes;
 	unsigned _bitsPerOp;
 	CimbDecoder _decoder;
 };
+
+inline Decoder::Decoder(unsigned ecc_bytes, unsigned bits_per_op)
+    : _eccBytes(ecc_bytes)
+    , _bitsPerOp(bits_per_op? bits_per_op : cimbar::Config::bits_per_cell())
+    , _decoder(cimbar::Config::symbol_bits(), cimbar::Config::color_bits())
+{
+}
+
+/* while bits == f.read_tile()
+ *     decode(bits)
+ *
+ * bitwriter bw("output.txt");
+ * img = open("foo.png")
+ * for tileX, tileY in img
+ *     bits = decoder.decode(img, tileX, tileY)
+ *     bw.write(bits)
+ *     if bw.shouldFlush()
+ *        bw.flush()
+ *
+ * */
+inline unsigned Decoder::do_decode(CimbReader& reader, unsigned ecc_bytes, std::string output, unsigned bits_per_op)
+{
+	bitwriter<> bw;
+	std::ofstream f(output);
+	reed_solomon_stream rss(f, ecc_bytes);
+
+	unsigned bytesWritten = 0;
+	while (!reader.done())
+	{
+		unsigned bits = reader.read();
+		bw.write(bits, bits_per_op);
+		if (bw.shouldFlush())
+			bytesWritten = bw.flush(rss);
+	}
+
+	// flush once more
+	bytesWritten = bw.flush(rss);
+	return bytesWritten;
+}
+
+// seems like we want to take a file or img, and have an output sink
+// output sync could be template param?
+// we'd decode the full message (via bit_writer) to a temp buffer -- probably a stringstream
+// then we'd direct the stringstream to our sink
+// which would either be a filestream, or a multi-channel fountain sink
+
+inline unsigned Decoder::decode(const cv::Mat& img, std::string output)
+{
+	CimbReader reader(img, _decoder);
+	return do_decode(reader, _eccBytes, output, _bitsPerOp);
+}
+
+inline unsigned Decoder::decode(std::string filename, std::string output)
+{
+	CimbReader reader(filename, _decoder);
+	return do_decode(reader, _eccBytes, output, _bitsPerOp);
+}
