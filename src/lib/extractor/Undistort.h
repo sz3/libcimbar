488f9a29ae53031ea6c1e433148f7c9949ab1334
sz <sz@galacticicecube.com>
2020-06-12 20:13:16 -0500

    Split undistort into initUndistortRectifyMap + remap.
    SPEEED

--- src/lib/extractor/Undistort.h
+++ src/lib/extractor/Undistort.h
@@ -7,13 +7,12 @@ template <typename CAMERA_CALIBRATOR>
 class Undistort
 {
 public:
-	Undistort()
-	    : _params()
-	{}
+	Undistort() {}
 
-	Undistort(const DistortionParameters& params)
-	    : _params(params)
-	{}
+	Undistort(int width, int height, const DistortionParameters& params)
+	{
+		set_distortion_params(width, height, params);
+	}
 
 	static DistortionParameters get_distortion_parameters(const cv::Mat& img)
 	{
@@ -24,20 +23,33 @@ public:
 	{
 		if (!_params)
 		{
-			_params = get_distortion_parameters(img);
-			if (!_params)
+			if ( !set_distortion_params(img.cols, img.rows, get_distortion_parameters(img)) )
 				return false;
 		}
 
-		cv::undistort(img, out, _params.camera, _params.distortion);
+		cv::remap(img, out, _map1, _map2, cv::INTER_LINEAR);
+		return true;
+	}
+
+	bool set_distortion_params(int width, int height, const DistortionParameters& params)
+	{
+		if (!params)
+			return false;
+
+		_params = params;
+		cv::initUndistortRectifyMap(_params.camera, _params.distortion, cv::Mat(), _params.camera, cv::Size(width, height), CV_32FC1, _map1, _map2);
 		return true;
 	}
 
 	void reset_distortion_params()
 	{
 		_params = {};
+		_map1.release();
+		_map2.release();
 	}
 
 protected:
 	DistortionParameters _params;
+	cv::Mat _map1;
+	cv::Mat _map2;
 };
