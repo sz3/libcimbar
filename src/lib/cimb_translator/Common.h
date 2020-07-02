afcd1fe85e54fa29111e6aa16fece80537a33780
sz <sz@galacticicecube.com>
2020-07-02 17:13:58 -0500

    Back out CIE76??

--- src/lib/cimb_translator/Common.h
+++ src/lib/cimb_translator/Common.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include "color-util/type.hpp"
 #include <opencv2/opencv.hpp>
 #include <string>
 
@@ -10,8 +9,6 @@ namespace cimbar
 
 	cv::Mat load_img(std::string path, const std::string& image_dir="");
 
-	colorutil::Lab convertColor(uchar r, uchar g, uchar b);
 	std::tuple<uchar,uchar,uchar> getColor(unsigned index);
-	colorutil::Lab getLabColor(unsigned index);
 	cv::Mat getTile(unsigned symbol_bits, unsigned symbol, bool dark=true, unsigned color=0, const std::string& image_dir="");
 }
