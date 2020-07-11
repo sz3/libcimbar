59e1be48479562a7c0f57be8dc94084caf615478
sz <sz@galacticicecube.com>
2020-07-10 21:21:09 -0500

    We begin to optimize by deoptimizing

---
+++ src/lib/cimb_translator/AdjacentCellFinder.cpp
@@ -0,0 +1,93 @@
+#include "AdjacentCellFinder.h"
+
+AdjacentCellFinder::AdjacentCellFinder(const CellPositions::positions_list& positions, int dimensions, int marker_size)
+    : _positions(positions)
+    , _markerSize(marker_size)
+    , _dimensions(dimensions)
+{
+	int midDimensions = dimensions - marker_size - marker_size;
+	int midCells = dimensions * midDimensions;
+	int edgeCells = midDimensions * marker_size;
+	_firstMid = edgeCells;
+	_firstBottom = edgeCells + midCells;
+}
+
+std::array<int, 4> AdjacentCellFinder::find(int index)
+{
+	std::array<int,4> adj = {
+	    right(index), left(index), bottom(index), top(index)
+	};
+	return adj;
+}
+
+int AdjacentCellFinder::dimensions() const
+{
+	return _dimensions;
+}
+
+int AdjacentCellFinder::marker_size() const
+{
+	return _markerSize;
+}
+
+int AdjacentCellFinder::in_row_with_margin(int index) const
+{
+	if (index < _firstMid)  // top
+		return 1;
+	if (index < _firstBottom)  // middle
+		return 0;
+	else
+		return 1; // bottom
+}
+
+int AdjacentCellFinder::right(int index) const
+{
+	int next = index+1;
+	if (next >= _positions.size())
+		return -1;
+	if (_positions[next].first < _positions[index].first)  // loop
+		return -1;
+	return next;
+}
+
+int AdjacentCellFinder::left(int index) const
+{
+	int next = index-1;
+	if (next < 0)
+		return -1;
+	if (_positions[next].first > _positions[index].first)  // loop
+		return -1;
+	return next;
+}
+
+int AdjacentCellFinder::bottom(int index) const
+{
+	int increment = _dimensions;
+	if (in_row_with_margin(index))
+		increment -= _markerSize;
+	int next = index + increment;
+	if (in_row_with_margin(next))
+		next -= _markerSize;
+
+	if (next >= _positions.size())
+		return -1;
+	if (_positions[next].first != _positions[index].first)  // near anchor
+		return -1;
+	return next;
+}
+
+int AdjacentCellFinder::top(int index) const
+{
+	int increment = _dimensions;
+	if (in_row_with_margin(index))
+		increment -= _markerSize;
+	int next = index - increment;
+	if (in_row_with_margin(next))
+		next += _markerSize;
+
+	if (next < 0)
+		return -1;
+	if (_positions[next].first != _positions[index].first)  // near anchor
+		return -1;
+	return next;
+}
---
+++ src/lib/cimb_translator/FloodDecodePositions.cpp
@@ -0,0 +1,62 @@
+#include "FloodDecodePositions.h"
+
+FloodDecodePositions::FloodDecodePositions(int spacing, int dimensions, int offset, int marker_size)
+    : _positions(CellPositions::compute(spacing, dimensions, offset, marker_size, 0))
+    , _cellFinder(_positions, dimensions, marker_size)
+{
+	reset();
+}
+
+size_t FloodDecodePositions::count() const
+{
+	return _positions.size();
+}
+
+void FloodDecodePositions::reset()
+{
+	_index = 0;
+	_remaining.clear();
+	for (int i = 0; i < _positions.size(); ++i)
+		_remaining.insert(i);
+
+	// seed
+	int smallRowLen = _cellFinder.dimensions() - (2*_cellFinder.marker_size()) - 1;
+	int lastElem = _positions.size()-1;
+	_heap.push({0, CellDrift(), 0});
+	_heap.push({smallRowLen, CellDrift(), 0});
+	_heap.push({lastElem, CellDrift(), 0});
+	_heap.push({lastElem-smallRowLen, CellDrift(), 0});
+}
+
+bool FloodDecodePositions::done() const
+{
+	return _remaining.empty();
+}
+
+FloodDecodePositions::iter FloodDecodePositions::next()
+{
+	while (!_heap.empty())
+	{
+		auto [i, drift, _] = _heap.top();
+		_heap.pop();
+		if (_remaining.erase(i))
+		    return {i, _positions[i], drift};
+	}
+
+	return {0, {0, 0}, CellDrift()};
+}
+
+int FloodDecodePositions::update(unsigned index, const CellDrift& drift, unsigned error_distance)
+{
+	std::array<int,4> adj = _cellFinder.find(index);
+	for (int next : adj)
+	{
+		if (next < 0)
+			continue;
+		if (_remaining.find(next) == _remaining.end())
+			continue;
+		_heap.push({next, drift, error_distance});
+	}
+	return 0;
+}
+
