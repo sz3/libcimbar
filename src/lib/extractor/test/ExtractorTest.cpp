30d1435d4aec80fb0ecf9edde802c53904b9e716
sz <sz@galacticicecube.com>
2020-07-21 01:30:05 -0500

    Update test hashes

--- src/lib/extractor/test/ExtractorTest.cpp
+++ src/lib/extractor/test/ExtractorTest.cpp
@@ -16,7 +16,7 @@ TEST_CASE( "ExtractorTest/testExtract", "[unit]" )
 	ext.extract(TestCimbar::getSample("4color1.jpg"), imgPath);
 
 	cv::Mat out = cv::imread(imgPath);
-	assertEquals( 0x4b4caa8597fc56c7, image_hash::average_hash(out) );
+	assertEquals( 0xe36a3fe9a15532d2, image_hash::average_hash(out) );
 }
 
 TEST_CASE( "ExtractorTest/testExtractMid", "[unit]" )
@@ -28,7 +28,7 @@ TEST_CASE( "ExtractorTest/testExtractMid", "[unit]" )
 	ext.extract(TestCimbar::getSample("4c-cam-40-f1.jpg"), imgPath);
 
 	cv::Mat out = cv::imread(imgPath);
-	assertEquals( 0xe0d2b532f6b9b270, image_hash::average_hash(out) );
+	assertEquals( 0xe4d9d6f4cad4b07, image_hash::average_hash(out) );
 }
 
 TEST_CASE( "ExtractorTest/testExtractUpscale", "[unit]" )
@@ -40,6 +40,6 @@ TEST_CASE( "ExtractorTest/testExtractUpscale", "[unit]" )
 	ext.extract(TestCimbar::getSample("4color-cam-140.jpg"), imgPath);
 
 	cv::Mat out = cv::imread(imgPath);
-	assertEquals( 0x204cc7f08dfd8f5c, image_hash::average_hash(out) );
+	assertEquals( 0x3af1bfb10fe33204, image_hash::average_hash(out) );
 }
 
