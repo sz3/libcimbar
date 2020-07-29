ce88b2925e08fba5334961668c9939fcb758b3e1
sz <sz@galacticicecube.com>
2020-07-29 10:53:48 -0500

    Moving concurrent_fountain_decoder_sink over from cfc.
    Probably will actually test it now? :o
    
    Includes concurrentqueue dependency.

---
+++ src/lib/fountain/concurrent_fountain_decoder_sink.h
@@ -0,0 +1,53 @@
+#pragma once
+
+#include "fountain_decoder_sink.h"
+
+#include "concurrentqueue/concurrentqueue.h"
+#include <mutex>
+
+class concurrent_fountain_decoder_sink
+{
+public:
+	concurrent_fountain_decoder_sink(std::string data_dir, unsigned chunk_size)
+		: _decoder(data_dir, chunk_size)
+	{
+	}
+
+	unsigned chunk_size() const
+	{
+		return _decoder.chunk_size();
+	}
+
+	unsigned num_streams() const
+	{
+		return _decoder.num_streams();
+	}
+
+	unsigned num_done() const
+	{
+		return _decoder.num_done();
+	}
+
+	void process()
+	{
+		if (_mutex.try_lock())
+		{
+			std::string buff;
+			while (_backlog.try_dequeue(buff))
+				_decoder << buff;
+			_mutex.unlock();
+		}
+	}
+
+	concurrent_fountain_decoder_sink& operator<<(const std::string& buffer)
+	{
+		_backlog.enqueue(buffer);
+		process();
+		return *this;
+	}
+
+protected:
+	std::mutex _mutex;
+	fountain_decoder_sink _decoder;
+	moodycamel::ConcurrentQueue< std::string > _backlog;
+};
