8b397ff8158513f9a106edfb64efb76baac299e8
sz <sz@galacticicecube.com>
2020-08-09 23:03:44 -0500

    Update/improve(?) command line options?
    cxxopts actually does provide some decent flexibility, so try using it.

--- src/exe/cimbar_scan/cimbar_scan.cpp
+++ src/exe/cimbar_scan/cimbar_scan.cpp
@@ -11,11 +11,14 @@ int main(int argc, char** argv)
 	cxxopts::Options options("cimbar scan", "Scan/extract cimbar code from a source image.");
 
 	options.add_options()
-		("i,in", "Encoded png/jpg/etc", cxxopts::value<std::string>())
-		("o,out", "Output image", cxxopts::value<std::string>())
-		("d,dark", "Dark mode (default: true)", cxxopts::value<bool>())
-		("h,help", "Print usage")
+	    ("i,in", "Encoded png/jpg/etc", cxxopts::value<std::string>())
+	    ("o,out", "Output image", cxxopts::value<std::string>())
+	    ("d,dark", "Dark mode", cxxopts::value<bool>()->default_value("true"))
+	    ("h,help", "Print usage")
 	;
+	options.show_positional_help();
+	options.parse_positional({"in", "out"});
+	options.positional_help("<in> <out>");
 
 	auto result = options.parse(argc, argv);
 	if (result.count("help") or !result.count("out") or !result.count("in"))
@@ -26,9 +29,7 @@ int main(int argc, char** argv)
 
 	std::string infile = result["in"].as<std::string>();
 	std::string outfile = result["out"].as<std::string>();
-	bool dark = true;
-	if (result.count("dark"))
-		dark = result["dark"].as<bool>();
+	bool dark = result["dark"].as<bool>();
 
 	Extractor ext;
 	if (!ext.extract(infile, outfile))
