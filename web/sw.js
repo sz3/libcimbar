e20a3cd75fa0e2135b22f534889fc304f0fd9e60
sz <sz@galacticicecube.com>
2021-06-12 21:21:09 -0500

    WIP? cache please?

--- web/sw.js
+++ web/sw.js
@@ -1,7 +1,6 @@
 
-var _cacheName = 'cimbar-js-pwa';
+var _cacheName = 'cimbar-js-v0.5.8';
 var _cacheFiles = [
-  '/',
   '/index.html',
   '/cimbar_js.js',
   '/cimbar_js.wasm',
@@ -25,4 +24,16 @@ self.addEventListener('fetch', function(e) {
       return response || fetch(e.request);
     })
   );
+});
+
+// clean old caches
+self.addEventListener('activate', function(e) {
+  e.waitUntil(caches.keys().then(function(keyList) {
+    Promise.all(keyList.map(function(key) {
+      if (key === _cacheName) {
+        return;
+      }
+      caches.delete(key);
+    }));
+  })());
 });
