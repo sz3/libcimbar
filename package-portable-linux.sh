c5d183c8b953274199af753be7443933c7fb607a
sz <sz@galacticicecube.com>
2022-02-18 18:56:05 -0600

    Ubuntu 16.04 build for glibc >= 2.23. Or 2.14(!?)
    objdump says we only need 2.14, and the cimbar binary runs on centos7...
    I see this as an absolute win

--- package-portable-linux.sh
+++ package-portable-linux.sh
@@ -1,17 +1,31 @@
 #!/bin/sh
-# docker run --mount type=bind,source="$(pwd)",target="/usr/src/app" -it ubuntu:18.04
+## targeting old glibc
+# docker run --mount type=bind,source="$(pwd)",target="/usr/src/app" -it ubuntu:16.04
 
 cd /usr/src/app
+
+# https://gist.github.com/jlblancoc/99521194aba975286c80f93e47966dc5
 apt update
-apt install -y pkgconf g++ cmake
+apt install -y software-properties-common
+add-apt-repository -y ppa:ubuntu-toolchain-r/test
+
+apt update
+apt install -y pkgconf g++-7 python-pip
 apt install -y libgles2-mesa-dev libglfw3-dev
+
+# cmake (via pip)
+python -m pip install cmake
+
+# use gcc7
+update-alternatives --install /usr/bin/cc cc /usr/bin/gcc-7 100
+update-alternatives --install /usr/bin/c++ c++ /usr/bin/g++-7 100
 
 cd opencv4/
 mkdir build-portable/ && cd build-portable/
-cmake .. -DCMAKE_BUILD_TYPE=RELEASE -DBUILD_SHARED_LIBS=OFF -DOPENCV_GENERATE_PKGCONFIG=YES -DOPENCV_FORCE_3RDPARTY_BUILD=YES
+/usr/local/bin/cmake .. -DCMAKE_BUILD_TYPE=RELEASE -DBUILD_SHARED_LIBS=OFF -DOPENCV_GENERATE_PKGCONFIG=YES -DOPENCV_FORCE_3RDPARTY_BUILD=YES
 make -j5 install
 
 cd /usr/src/app
 mkdir build-portable/ && cd build-portable/
-cmake .. -DBUILD_PORTABLE_LINUX=1
+/usr/local/bin/cmake .. -DBUILD_PORTABLE_LINUX=1
 make -j5 install
