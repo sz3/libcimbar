6b8778ea1b92e6f66bb76652e4bf39f3e380e47c
sz <sz@recv.cc>
2024-02-03 07:08:30 -0600

    Less && in package-wasm, so we can re-run the script with (some) impunity
    There are more efficient ways to do incremental builds, but this is
    convenient.

--- package-wasm.sh
+++ package-wasm.sh
@@ -7,17 +7,20 @@ apt update
 apt install python3 -y
 
 cd opencv4/
-mkdir opencv-build-wasm && cd opencv-build-wasm
+mkdir opencv-build-wasm
+cd opencv-build-wasm
 python3 ../platforms/js/build_js.py build_wasm --build_wasm --emscripten_dir=/emsdk/upstream/emscripten
 
 cd /usr/src/app
-mkdir build-wasm && cd build-wasm
+mkdir build-wasm
+cd build-wasm
 emcmake cmake .. -DUSE_WASM=1 -DOPENCV_DIR=/usr/src/app/opencv4
 make -j5 install
 (cd ../web/ && tar -czvf cimbar.wasm.tar.gz cimbar_js.* index.html main.js)
 
 cd /usr/src/app
-mkdir build-asmjs && cd build-asmjs
+mkdir build-asmjs
+cd build-asmjs
 emcmake cmake .. -DUSE_WASM=2 -DOPENCV_DIR=/usr/src/app/opencv4
 make -j5 install
 (cd ../web/ && zip cimbar.asmjs.zip cimbar_js.js index.html main.js)
