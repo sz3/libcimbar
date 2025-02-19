1f055efb3abf1e8fe2e3431af204f7f8102c2173
sz <sz@recv.cc>
2025-02-19 03:36:48 -0600

    Add test case to validate fountain decode works for various inputs
    It's not truly *random* inputs (the test uses a repeating pattern), but
    running this in a loop does give some confidence that magically bad
    inputs.
    
    Also adding some more gitignore entries

---
+++ src/lib/fountain/test/fountain_sinkSpecialTest.cpp
@@ -0,0 +1,93 @@
+/* This code is subject to the terms of the Mozilla Public License, v.2.0. http://mozilla.org/MPL/2.0/. */
+#include "unittest.h"
+
+#include "FountainMetadata.h"
+#include "fountain_encoder_stream.h"
+#include "fountain_decoder_sink.h"
+
+#include "serialize/format.h"
+#include "util/File.h"
+#include "util/MakeTempDirectory.h"
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+
+using std::string;
+using namespace std;
+
+namespace {
+	stringstream dummyContents(unsigned size, string contents="0123456789")
+	{
+		stringstream input;
+		for (unsigned i = 0; i < (size/contents.size()); ++i)
+			input << contents;
+		return input;
+	}
+
+	string createFrame(fountain_encoder_stream& fes)
+	{
+		stringstream ss;
+		std::array<char, 125> buff;
+
+		// for a 7500 byte frame, 125*60 does the trick
+		for (int i = 0; i < 60; ++i)
+		{
+			unsigned res = fes.readsome(buff.data(), buff.size());
+			assertEquals( res, buff.size() );
+			ss << string(buff.data(), buff.size());
+		}
+		return ss.str();
+	}
+
+	std::string random_string(unsigned len, std::string chars="abcdefghijklmnaoqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890")
+	{
+		std::cout << ::rand() << std::endl;
+		std::string output = "";
+		for (unsigned i = 0; i < len; ++i)
+		{
+			unsigned idx = ::rand() % chars.length();
+			output += chars[idx];
+		}
+		return output;
+	}
+}
+
+TEST_CASE( "FountainSinkSpecialTest/testMultipart", "[unit]" )
+{
+	::srand( ::time(nullptr) );
+	MakeTempDirectory tempdir;
+
+	fountain_decoder_sink<std::ofstream> sink(tempdir.path(), 750);
+
+	const int totalSize = 6000000;
+	string randostr = random_string(2500);
+	std::cout << randostr << std::endl;
+	stringstream input = dummyContents(totalSize, randostr);
+	fountain_encoder_stream::ptr fes = fountain_encoder_stream::create(input, 750, 108);
+
+	for (int i = 0; i < (totalSize / 7500) * 3; ++i)
+	{
+		string iframe = createFrame(*fes);
+		assertEquals( 7500, iframe.size() );
+
+		FountainMetadata md(iframe.data(), iframe.size());
+		assertEquals( totalSize, md.file_size() );
+		assertEquals( 108, md.encode_id() );
+
+		if (i % 2 == 1)
+		{
+			bool res = sink.decode_frame(iframe.data(), iframe.size());
+			//std::cout << i << ": " << res << std::endl;
+			assertMsg( ((i == 1613) == res), fmt::format("failed {}", i) );
+			assertMsg( ((i >= 1613) == sink.is_done(md.id())), fmt::format("failed {}", i) );
+		}
+	}
+
+	assertEquals( 0, sink.num_streams() );
+	assertEquals( 1, sink.num_done() );
+
+	string contents = File(tempdir.path() / fmt::format("108.{}", totalSize)).read_all();
+	assertEquals( totalSize, contents.size() );
+	assertEquals( input.str(), contents );
+}
