1a1cd2a59432ac34c5bf42bd618f40f6f5ad17e2
WuYJ <1427760@qq.com>
2025-03-03 19:55:54 +0800

    Undo incorrect changes made to the upstream fmt

--- src/third_party_lib/fmt/format.h
+++ src/third_party_lib/fmt/format.h
@@ -1453,11 +1453,7 @@ template <typename WChar, typename Buffer = memory_buffer> class to_utf8 {
         ++p;
         if (p == s.end() || (c & 0xfc00) != 0xd800 || (*p & 0xfc00) != 0xdc00) {
           if (policy == to_utf8_error_policy::abort) return false;
-#ifdef _WIN32
-          buf.append(string_view("\xef\xbf\xbd"));
-#else
           buf.append(string_view("�"));
-#endif
           --p;
         } else {
           c = (c << 10) + static_cast<uint32_t>(*p) - 0x35fdc00;
