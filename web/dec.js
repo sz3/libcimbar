83717f8b8734e225778298741518bf751ec2a983
sz <sz@recv.cc>
2025-05-30 22:37:29 -0500

    stub impl for web-based decoder

---
+++ web/dec.js
@@ -0,0 +1,126 @@
+var Dec = function() {
+
+var _counter = 0;
+var _renderTime = 0;
+var _video = 0;
+
+var _workers = [];
+var _nextWorker = 0;
+
+function toggleFullscreen()
+{
+  if (document.fullscreenElement) {
+    return document.exitFullscreen();
+  }
+  else {
+    return document.documentElement.requestFullscreen();
+  }
+}
+
+// public interface
+return {
+  init : function(video, num_workers)
+  {
+    Dec.init_video(video);
+	Dec.init_ww(num_workers);
+  },
+
+  set_error : function(msg)
+  {
+    Dec.set_HTML('errorbox', msg);
+    return false;
+  },
+
+  init_ww : function(num_workers)
+  {
+	// clean up _workers if exists?
+	_workers = [];
+	for (let i = 0; i < num_workers; i++)
+	{
+		_workers.push(new Worker('dec-worker.js'));
+
+		_workers[i].onmessage = (event) => {
+			console.log('Main thread received message from worker' + i + ':', event.data);
+			Dec.set_HTML("t" + i, "avg red " + i + " is " + event.data.res);
+		};
+
+		_workers[i].onerror = (error) => {
+			console.error('Worker' + i + ' error:', error);
+		};
+	}
+  },
+
+  init_video : function(video)
+  {
+    _video = video;
+    // ios nonsense
+    video.setAttribute('autoplay', '');
+    video.setAttribute('muted', '');
+    video.setAttribute('playsinline', '');
+
+    var constraints = {
+        audio: false,
+        video: {
+          	facingMode: 'environment',
+            width: { ideal: 1080 }, // Request HD but allow flexibility
+            height: { ideal: 1080 },
+        }
+    };
+
+    if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia)
+    {
+        return Dec.set_error('mediaDevices not supported? :(');
+    }
+
+    navigator.mediaDevices.getUserMedia(constraints)
+    .then(localMediaStream => {
+      console.log(localMediaStream);
+      console.dir(video);
+      if ('srcObject' in video) {
+        video.srcObject = localMediaStream;
+      } else {
+        video.src = URL.createObjectURL(localMediaStream); //deprecated
+      }
+      video.play();
+      video.requestVideoFrameCallback(Dec.on_frame);
+    })
+    .catch(err => {
+      console.error(`OH NO!!!!`, err);
+    });
+
+  },
+
+  on_frame : function(now, metadata)
+  {
+    console.log("on frame");
+    // https://developer.mozilla.org/en-US/docs/Web/API/VideoFrame
+
+	if (_workers.length == 0)
+		return;
+	if (_nextWorker >= _workers.length)
+		_nextWorker = 0;
+
+	try {
+		const vf = new VideoFrame(_video);
+		_workers[_nextWorker].postMessage({ type: 'proc', vf: vf, width: _video.videoWidth, height: _video.videoHeight });
+	} catch (e) {
+        console.log(e);
+    }
+
+    // schedule the next one
+	_nextWorker += 1;
+    _video.requestVideoFrameCallback(Dec.on_frame);
+  },
+
+  set_HTML : function(id, msg)
+  {
+    document.getElementById(id).innerHTML = msg;
+  },
+
+  set_title : function(msg)
+  {
+    document.title = "Cimbar: " + msg;
+  }
+};
+}();
+
