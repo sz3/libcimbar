df8151c379e649dce932af43441d695dece44d7f
sz <sz@recv.cc>
2025-06-02 21:42:02 -0500

    Allocate some buffers + allow reconfig + lazy sink create
    Since only the fountain decode thread will care about having a
    decoder_sink, we can (and perhaps should) have the config method clear
    the old one and have the decode call (if it happens on our wasm
    thread...) create the new one.

--- web/dec-worker.js
+++ web/dec-worker.js
@@ -1,5 +1,5 @@
 let _wasmInitialized = false;
-let _buff = undefined;
+let _buffs = {};
 
 var Module = {
     preRun:[],
@@ -7,6 +7,40 @@ var Module = {
         console.info("The Module is loaded and is accessible here", Module);
         _wasmInitialized = true;
     },
+	mallocPlease : function(name, size)
+	{
+		if (_buffs[name] === undefined || size > _buffs[name].length)
+        {
+			try {
+            	if (size > _buffs[name].length)
+				{
+					console.log("resizing " + name + " buff from " + _buffs[name].length + " to " + size);
+					Module._free(_buffs[name].byteOffset);
+				}
+			} catch (e) {} // if we're leaking memory we'll find out the hard way 
+            const dataPtr = Module._malloc(size);
+            _buffs[name] = new Uint8Array(Module.HEAPU8.buffer, dataPtr, size);
+        }
+	},
+
+	mallocAll : function(vf)
+	{
+		const imgsize = vf.allocationSize({format: "RGBX"});
+		Module.mallocPlease("img", imgsize);
+
+		const bufsize = Module._fountain_get_bufsize();
+		Module.mallocPlease("fountain", bufsize);
+	},
+
+	imgBuff : function()
+	{
+		return _buffs['img'];
+	},
+
+	fountainBuff : function()
+	{
+		return _buffs['fountain'];
+	}
 };
 
 importScripts('cimbar_js.js');
@@ -19,21 +53,28 @@ self.onmessage = async (event) => {
 		self.postMessage({ type: 'startWasm', error: "no wasm" });
 		return;
 	}
+
+	if (event.data.config)
+	{
+		console.log('attempting config');
+		try {
+			Module._configure_decode(event.config.color_bits, event.config.mode_val);
+		} catch (e) {
+			self.postMessage({ type: 'config', error: e });
+		}
+		return;
+	}
 	
 	const vf = event.data.vf;
 	const width = event.data.width;
 	const height = event.data.height;
 	try {
         console.log(vf);
-        if (_buff === undefined)
-        {
-            const size = vf.allocationSize({format: "RGBX"});
-            const dataPtr = Module._malloc(size);
-            _buff = new Uint8Array(Module.HEAPU8.buffer, dataPtr, size);
-        }
-        vf.copyTo(_buff, {format: "RGBX"});
+		// malloc iff necessary
+        Module.mallocAll(vf);
+        vf.copyTo(Module.imgBuff(), {format: "RGBX"});
         // then decode in wasm, fool
-        var avg = Module._do_decode(_buff.byteOffset, width, height);
+        var avg = Module._do_decode(Module.imgBuff().byteOffset, width, height);
         self.postMessage({ type: 'proc', res: avg });
     } catch (e) {
         console.log(e);
