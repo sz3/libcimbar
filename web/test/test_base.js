ea9ae97aa120614ed03be3288a091db1196ba947
sz <sz@recv.cc>
2025-07-17 22:59:22 -0500

    finish very basic first pass at test
    Want a more complete example, but this does cover a fair bit

--- web/test/test_base.js
+++ web/test/test_base.js
@@ -1,6 +1,33 @@
 QUnit.module( "base" );
 QUnit.config.reorder = false;
 
+function wait_for(assert, block)
+{
+  var done = assert.async();
+  return new Promise(resolve => {
+    // A function that checks the condition.
+    const check = () => {
+      // If the condition is met, resolve the promise.
+      var res;
+      try {
+        res = block();
+      } catch (ex) {
+        assert.ok(false, ex);
+      }
+      if (res) {
+        done();
+        resolve(res);
+      } else {
+        // schedule the next check.
+        requestAnimationFrame(check);
+      }
+    };
+
+    check();
+  });
+}
+
+
 QUnit.testStart(async function(details) {
   const img = document.getElementById('example_frame');
   const imageBitmap = await window.createImageBitmap(img);
@@ -18,11 +45,18 @@ QUnit.testDone(function(details) {
 
 });
 
-QUnit.test( "simple", async function( assert ) {
+QUnit.test( "stable decode", async function( assert ) {
 
-  console.log("IT'S GO TIME")
-  
   Recv.on_frame(0,'');
-  assert.ok( true);
-  assert.equal( 1, 2 );
+
+  const progress_container = document.getElementById('progress_bars');
+	const query = '#progress_bars > div[class="progress"]';
+
+  const progress = await wait_for(assert, () => {
+    return document.querySelector(query);
+  });
+
+
+  assert.ok( progress);
+  assert.equal( progress.style.width, "30.7692%" );
 });
