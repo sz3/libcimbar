21bbf57cf574c56292eb5fddf870d88b242ebd26
sz <sz@recv.cc>
2025-07-25 02:26:51 -0500

    Taking another swing at the wasm cache-busting packaging script

--- web/wasmgz.sh
+++ web/wasmgz.sh
@@ -2,21 +2,33 @@
 # run from within web dir
 # does various renames for cache busting
 
-VERSION=${VERSION:-$(date --iso-8601)}
+RENAME_FILES=$(ls cimbar_js.js cimbar_js.wasm main.js recv*.js zstd.js sw*.js pwa*.json)
+GSUB_FILES="index.html recv.html $(echo $RENAME_FILES | sed 's/cimbar_js\.wasm//g')"
+
+VERSION=${VERSION:-$(date --iso-8601=hours --utc | awk -F+ '{print $1}')}
 echo $VERSION
 
 mkdir $VERSION
 cd $VERSION
 
-cp ../cimbar_js.wasm cimbar_js-$VERSION.wasm
-sed "s/cimbar_js.wasm/cimbar_js-$VERSION.wasm/g" ../cimbar_js.js > cimbar_js-$VERSION.js
-sed "s/cimbar_js.js/cimbar_js-$VERSION.js/g" ../main.js > main-$VERSION.js
+for f in $(echo $RENAME_FILES $GSUB_FILES | xargs -n 1 | sort -u); do
+	cp ../$f $f
+done
 
-sed "s/main.js/main-$VERSION.js/g" ../index.html | sed "s/cimbar_js.js/cimbar_js-$VERSION.js/g" > index.html
+for f in $(echo $GSUB_FILES | xargs -n 1 | sort -u); do
+	for ren in $(echo $RENAME_FILES | xargs -n 1 | sort -u); do
+		renew=$(echo $ren"."$VERSION | awk -F. '{print $1 "." $3 "." $2}')
+		sed -i "s/$ren/$renew/g" $f
+	done
+done
 
-sed "s/cimbar_js.js/cimbar_js-$VERSION.js/g" ../dec-worker.js > dec-worker-$VERSION.js
-sed "s/cimbar_js.js/cimbar_js-$VERSION.js/g" ../dec.js | sed "s/dec-worker.js/dec-worker-$VERSION.js/g" > dec-$VERSION.js
+for f in $(ls sw*.js); do
+	sed -i "s/VERSION/$VERSION/g" $f
+done
 
-sed "s/dec.js/dec-$VERSION.js/g" ../dec.html | sed "s/cimbar_js.js/cimbar_js-$VERSION.js/g" > dec.html
+for f in $(echo $RENAME_FILES | xargs -n 1 | sort -u); do
+	newname=$(echo $f"."$VERSION | awk -F. '{print $1 "." $3 "." $2}')
+	mv $f $newname
+done
 
 tar -czvf ../cimbar.wasm.tar.gz *
